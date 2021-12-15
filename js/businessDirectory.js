0fb7723167dda512a0c98db7a45494870c361896
dandlp95 <del17011@byui.edu>
2021-12-15 02:58:47 -0700

    progress

--- js/businessDirectory.js
+++ js/businessDirectory.js
@@ -13,7 +13,6 @@ fetch(directoryUrl)
         for(let i = 0; i<businessDirectory.length; i++) {
             
             let businessSection = document.createElement("section");
-
             let businessName = document.createElement("h3");
             let businessLogo = document.createElement("img");
             let businessContactInformation = document.createElement("div");
@@ -21,27 +20,39 @@ fetch(directoryUrl)
             let businessAddress = document.createElement("p");
             let businessEmail = document.createElement("p");
             let businessHours = document.createElement("p");
-            let businessWebsite = document.createElement("p");
+            let businessWebsite = document.createElement("a");
 
             businessName.textContent = businessDirectory[i].name;
             businessLogo.setAttribute("src", businessDirectory[i].logo)
             
-            businessPhoneNumber.textContent = businessDirectory[i].phone;
-            businessAddress.textContent = businessDirectory[i].address;
-            businessEmail.textContent = businessDirectory[i].email;
+            businessPhoneNumber.textContent = businessDirectory[i].contact.phone;
+            businessAddress.textContent = businessDirectory[i].contact.address;
+            businessEmail.textContent = businessDirectory[i].contact.email;
+            
+            console.log(businessDirectory[i].phone);
 
             businessHours.textContent = businessDirectory[i].hours;
             businessWebsite.textContent = businessDirectory[i].website;
+
+            businessWebsite.setAttribute("href", businessDirectory[i].website);
 
             businessContactInformation.appendChild(businessPhoneNumber);
             businessContactInformation.appendChild(businessAddress);
             businessContactInformation.appendChild(businessEmail);
 
+            //console.log(businessContactInformation);
+            businessSection.setAttribute("class", "businessContainer")
             businessSection.appendChild(businessName);
             businessSection.appendChild(businessLogo);
             businessSection.appendChild(businessContactInformation);
             businessSection.appendChild(businessHours);
             businessSection.appendChild(businessWebsite);
+
+            businessName.setAttribute("class", "businessName");
+            businessLogo.setAttribute("class", "businessLogo");
+            businessContactInformation.setAttribute("class", "businessContactInformation");
+            businessHours.setAttribute("class", "businessHours");
+            businessWebsite.setAttribute("class", "businessWebsite");
 
             document.querySelector(".directoryContainer").appendChild(businessSection);
             
